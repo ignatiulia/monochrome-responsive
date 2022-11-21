a5c8ac84f53cb8f477ddcf74ef3f9dbd011297b4
ignatiulia <ignatiulia71@gmail.com>
2022-11-21 19:46:53 +0200

    21.11

--- pages/indexsetup.jsx
+++
@@ -1,5 +0,0 @@
-<div>
-  <button className="button button-primary">View all</button>
-  <button className="button button-secondary button-small">Submit</button>
-  <button className="button button-primary button-small">New in</button>
-</div>;
